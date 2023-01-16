9b5cf08e27693af2f21bb3c3a118629eb626ed3c
Bushuev Nikita <73890426+mrbushik@users.noreply.github.com>
2023-01-16 12:13:21 +0300

    create resume page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Route, Switch, Redirect } from "react-router-dom";
 
 import NavBar from "./Components/navigation/navBar";
 import MainPage from "./Components/pages/mainPage";
+import ResumePage from "./Components/pages/resumePage";
 
 function App() {
   return (
     <>
       <NavBar />
       <Switch>
+          <Route exact path="/resume" component={ResumePage} />
         <Route path="/" component={MainPage} />
         <Redirect to="/" />
       </Switch>
---
+++ src/Components/pages/resumePage.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ResumePage: React.FC = () => {
+  return (
+    <div>
+      <div className="resume-header">
+        <div className="resume-header__container">
+          {" "}
+          <h2>Nikita</h2>
+          <p className="resume-suriname">Bushuev</p>
+          <p className="resume-subtitle">Frontend developer</p>
+        </div>
+      </div>
+      <div className="container"></div>
+    </div>
+  );
+};
+
+export default ResumePage;
